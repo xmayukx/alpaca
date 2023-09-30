2d56f2d1593357e056493f79309ede8228743f55
Mayukh Hazari <hazari.mayukh77@gmail.com>
2023-09-30 18:06:24 +0530

    Add home page & initial files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,38 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import { Kanit } from "next/font/google";
+import { UserButton, auth } from "@clerk/nextjs";
+const kanit = Kanit({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 export default function Home() {
-  return <main className="">Hello World</main>;
+  const { userId } = auth();
+  const isAuth = !!userId;
+  return (
+    <main className="w-screen min-h-screen bg-black text-white">
+      <div
+        className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2`}
+      >
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <span
+              className={`mr-3 lg:text-6xl md:text-4xl text-2xl font-semibold ${kanit.className}`}
+            >
+              Talk to your PDF
+            </span>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          <p className=" max-w-xl mt-1 text-sm text-slate-400/75">
+            Join a vast community of students, researchers, and professionals to
+            promptly find answers to questions and gain insights from research
+            using AI technology.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
